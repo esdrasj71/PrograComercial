09fce52d4ad16a8d52d98d5099e0a4b268724ab9
Jimenez Esdras <esdrasj71@gmail.com>
2020-09-26 15:08:22 -0600

    Se separo el diseno en bd se agrego css se agrego detalles de la publicacion

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils import timezone
 from .models import Post
 
 # Create your views here.
 def post_list(request):
-    Posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
-    return render(request, 'blog/post_list.html', {'Posts':Posts})
+    posts = Post.objects.filter(fecha_publicacion__lte=timezone.now()).order_by('fecha_publicacion')
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
